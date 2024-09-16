73a2461db6f822caf4da0465c9a27485222e1e2b
Parth <anandk291@gmail.com>
2024-09-15 20:42:26 -0400

    made the link better for mobile

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function About() {
     return (
-        <header style={{ position: "absolute", top: 0, left: 0, padding: "10px"}}>
+        <header className="about-header">
             <a href="https://github.com/parthsinha1/react_todo" className="about-link">
                 Github Repo
             </a>
