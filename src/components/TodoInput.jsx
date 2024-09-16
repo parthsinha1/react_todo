75fb1663ec471817328e73c0b917bbd9705251f3
Parth <anandk291@gmail.com>
2024-09-15 20:42:59 -0400

    press enter instead of clicking on Add button

--- src/components/TodoInput.jsx
+++ src/components/TodoInput.jsx
@@ -2,17 +2,27 @@
 export default function TodoInput(props) {
   const { handleAddTodos, todoValue, setTodoValue } = props
 
+  const handleKeyDown = (event) => {
+    if (event.key === 'Enter') {
+      handleAddTodos(todoValue);
+      setTodoValue('');
+    }
+  };
+
 
 
   return (
     <header>
-        <input value={todoValue} onChange={(e) => {
-          setTodoValue(e.target.value)
-          }} placeholder="Enter Todo..." />
+        <input value={todoValue} 
+        onChange={(e) =>  setTodoValue(e.target.value)} 
+        onKeyDown={handleKeyDown} 
+        placeholder="Enter Todo..." />
+        
         <button onClick={() => {
           handleAddTodos(todoValue)
           setTodoValue('')
-          }}>Add</button>
+          }}>Add
+          </button>
 
     </header>
   )
